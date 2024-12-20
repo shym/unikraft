424eb3d920bfbe9ca0dc269750fba4a989ead184
Razvan Deaconescu <razvand@unikraft.io>
2024-12-20 23:36:51 +0200

    Release: v0.18.0 Helene
    Signed-off-by: Michalis Pappas <michalis@unikraft.io>
    Signed-off-by: Simon Kuenzer <skuenzer@unikraft.io>
    Signed-off-by: Andrei Tatar <andrei@unikraft.io>
    Signed-off-by: Cezar Craciunoiu <cezar.craciunoiu@unikraft.io>
    Signed-off-by: Stefan Jumarea <stefanjumarea02@gmail.com>
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>

--- version.mk
+++ version.mk
@@ -1,5 +1,5 @@
 UK_VERSION = 0
-UK_SUBVERSION = 17
+UK_SUBVERSION = 18
 UK_EXTRAVERSION = 0
-UK_CODENAME = Calypso
+UK_CODENAME = Helene
 # https://en.wikipedia.org/wiki/Moons_of_Saturn (by discovery year)
